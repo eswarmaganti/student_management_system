454107f09d7620d760dbf712f313f1887740b79d
Eswar Krishna Maganti <eswarmaganti@Eswars-MacBook-Air.local>
2024-05-22 20:47:39 +0530

    Initial Commit: created student dictionary structure

---
+++ main.py
@@ -0,0 +1,38 @@
+# student dictionary structure
+
+student = {
+    "name" : "",
+    "dob" : "", # dd-mm-yyyy
+    "age" : "", # we have to calculate
+    "aadhar": "",
+    "caste": "",
+    "class":"",
+    "section":"", # we have to calculate
+    "admission_number":"", # we have to calculate
+    "address": {
+        "village":"",
+        "mandal":"",
+        "district":"",
+        "state":""
+    },
+    "parents_details":{
+        "father":{
+            "name":"",
+            "occupation":"",
+            "age":"",
+            "aadhar":"",
+            "phone":""
+        },
+        "mother":{
+            "name":"",
+            "occupation":"",
+            "age":"",
+            "aadhar":"",
+            "phone":""
+        }
+    }
+}
+
+
+def get_student_details():
+    pass
