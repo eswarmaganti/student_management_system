17cfc3b239cadafff7184ce29cf6f3980c488447
Eswar Krishna Maganti <eswarmaganti@Eswars-MacBook-Air.local>
2024-05-24 20:49:35 +0530

    Created Register Student & Print Student Details Functions

--- main.py
+++ main.py
@@ -1,38 +1,68 @@
-# student dictionary structure
+import json
+from pprint import pprint
 
-student = {
-    "name" : "",
-    "dob" : "", # dd-mm-yyyy
-    "age" : "", # we have to calculate
-    "aadhar": "",
-    "caste": "",
-    "class":"",
-    "section":"", # we have to calculate
-    "admission_number":"", # we have to calculate
-    "address": {
-        "village":"",
-        "mandal":"",
-        "district":"",
-        "state":""
-    },
-    "parents_details":{
-        "father":{
-            "name":"",
-            "occupation":"",
-            "age":"",
-            "aadhar":"",
-            "phone":""
-        },
-        "mother":{
-            "name":"",
-            "occupation":"",
-            "age":"",
-            "aadhar":"",
-            "phone":""
-        }
-    }
-}
+# function to load student data from json file
+def load_student_data_from_file():
+    stream = open("data/student_data.json")
+    return json.load(stream)
 
 
+# get the student admission number
+def get_admission_number():
+    student_count = len(students_data.keys())+1
+    curr_student_number = f'{student_count}'.zfill(4)
+    admission_number = '2024'+curr_student_number
+
+    return admission_number
+
+
+# read the student data from keyboard
 def get_student_details():
-    pass
+    print("\n -- ENTER STUDENT DETAILS -- \n")
+    name = input("Enter student name: ")
+    dob = input("Enter student Date of Birth: ")
+    aadhar = input("Enter student aadhar: ")
+    std = input("Enter student class: ")
+    admission_number = get_admission_number()
+    
+    # TODO: Read Remaining fields
+    
+    return {"name":name,
+            "dob":dob,
+            "aadhar":aadhar,
+            "class":std,
+            "admission_number":admission_number
+            }
+
+# function to register a student to organization
+def register_student():
+    student = get_student_details()
+    admission_number = student['admission_number']
+    students_data[admission_number] = student
+
+    # saving the student data to a file
+    stream = open("data/student_data.json","w+")
+    json.dump(students_data,stream)
+    stream.close()
+    
+
+def print_student_detais():
+    admission_number = input("Enter Student Admission Number: ")
+    student = students_data.get(admission_number)
+    
+    #Todo: handle errors invalid admission number
+
+    print("\n-- STUDENT DETAILS --\n")
+    print(f"Name: {student.get('name')}")
+    print(f"Date of Birth: {student.get('dob')}")
+    print(f"Aadhar: {student.get('aadhar')}")
+    print(f"Class: {student.get('class')}")
+    
+    # TODO: update the code for remaining fields
+
+# main starts here
+if __name__ == "__main__":
+    # load existing student data
+    students_data = load_student_data_from_file()
+
+    #TODO:  create a menu 
---
+++ student_format.py
@@ -0,0 +1,34 @@
+# student dictionary structure
+
+student = {
+    "name" : "",
+    "dob" : "", # dd-mm-yyyy
+    "age" : "", # we have to calculate
+    "aadhar": "",
+    "caste": "",
+    "class":"",
+    "section":"", # we have to calculate–
+    "admission_number":"", # we have to calculate
+    "address": {
+        "village":"",
+        "mandal":"",
+        "district":"",
+        "state":""
+    },
+    "parents_details":{
+        "father":{
+            "name":"",
+            "occupation":"",
+            "age":"",
+            "aadhar":"",
+            "phone":""
+        },
+        "mother":{
+            "name":"",
+            "occupation":"",
+            "age":"",
+            "aadhar":"",
+            "phone":""
+        }
+    }
+}
