f1a3a0ba085c333ace9323261cd84847a967b456
Eswar Krishna Maganti <eswarmaganti@Eswars-MacBook-Air.local>
2024-05-28 08:29:41 +0530

    Feature: Print all student details implemented

--- main.py
+++ main.py
@@ -25,19 +25,44 @@ def get_student_details():
     std = input("Enter student class: ")
     admission_number = get_admission_number()
     
+    print("\nAddress Details")
+    village = input("Enter village name: ")
+    mandal = input("Enter mandal name: ")
+    district = input("Enter district name: ")
+    state = input("Enter state name: ")
+
+    print("\nParent Details")
+    father_name = input("Enter father's name: ")
+    father_occupation = input("Enter father's occupation: ")
+
     # TODO: Read Remaining fields
     
     return {"name":name,
             "dob":dob,
             "aadhar":aadhar,
             "class":std,
-            "admission_number":admission_number
+            "admission_number":admission_number,
+            "address": {
+                "village": village,
+                "mandal": mandal,
+                "district": district,
+                "state": state
+            },
+            "parent_details":{
+                "father":{
+                    "name": father_name,
+                    "occupation": father_occupation,
+                }
+            }
             }
 
 # function to register a student to organization
 def register_student():
+    
     student = get_student_details()
-    admission_number = student['admission_number']
+
+    # add new student to student_data
+    admission_number = student['admission_number'] # student.get("admission_number")
     students_data[admission_number] = student
 
     # saving the student data to a file
@@ -46,12 +71,16 @@ def register_student():
     stream.close()
     
 
+# function to print a student details
 def print_student_detais():
     admission_number = input("Enter Student Admission Number: ")
-    student = students_data.get(admission_number)
-    
-    #Todo: handle errors invalid admission number
 
+    #check whether the admission number is valid or invalid
+    if admission_number not in students_data.keys():
+        print(f"*** Error: Invalid admission number : {admission_number} entered ***")
+        return
+    
+    student = students_data.get(admission_number)
     print("\n-- STUDENT DETAILS --\n")
     print(f"Name: {student.get('name')}")
     print(f"Date of Birth: {student.get('dob')}")
@@ -60,9 +89,38 @@ def print_student_detais():
     
     # TODO: update the code for remaining fields
 
+# functions to print all student details
+def print_all_students_details():
+    for student in students_data.values():
+        print(f"\n-- STUDENT DETAILS of {student.get('admission_number')} --\n")
+        print(f"Name: {student.get('name')}")
+        print(f"Date of Birth: {student.get('dob')}")
+        print(f"Aadhar: {student.get('aadhar')}")
+        print(f"Class: {student.get('class')}")
+
 # main starts here
 if __name__ == "__main__":
     # load existing student data
     students_data = load_student_data_from_file()
 
     #TODO:  create a menu 
+    print(" STUDENT MANAGEMENT SYSTEM")
+    print()
+    print("1. Register New Student")
+    print("2. Print Student Details")
+    print("3. Print All Students Details")
+    print("4. Update Student Details")
+    print("5. Delete Student")
+
+    choice =  int(input("Enter your choice: "))
+
+    if choice == 1:
+        register_student()
+    elif choice ==2 :
+        print_student_detais()
+    elif choice == 3:
+        print_all_students_details()
+    elif choice == 4:
+        pass
+    else:
+        print("*** Error: Invalid choice entered as input ***")
